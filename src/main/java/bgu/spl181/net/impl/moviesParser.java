a5ea057b7efb1cc7eede3571644ee8607657368c
Yanir Oz Sahor <yanir@yanir-VirtualBox>
2018-01-08 11:39:24 +0200

    Json parsing

--- src/main/java/bgu/spl181/net/impl/moviesParser.java
+++ src/main/java/bgu/spl181/net/impl/moviesParser.java
@@ -1,6 +1,5 @@
 package bgu.spl181.net.impl;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -13,52 +12,10 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
-public class jsonParser {
+public class moviesParser {
 
 	private static MovieDataBase movieDataBase = MovieDataBase.getInstance();
-	private static UsersDataBase userDataBase = UsersDataBase.getInstance();
-
-	public static void parseUsersJSON() {
-
-		JSONParser parser = new JSONParser();
-		File srcFile = new File("Database/example_Users.json");
-		Reader reader = null;
-		try {
-			reader = new FileReader(srcFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		Object jsonObj = null;
-		try {
-			jsonObj = parser.parse(reader);
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-
-		JSONObject jsonObject = (JSONObject) jsonObj;
-		JSONArray usersData = (JSONArray) jsonObject.get("users");
-
-		for (int i = 0; i < usersData.size(); i++) {
-
-			JSONObject currentUser = (JSONObject) usersData.get(i);
-			String userName = currentUser.get("username").toString();
-			String password = currentUser.get("password").toString();
-			String country = currentUser.get("country").toString();
-			boolean isAdmin = currentUser.get("type").toString().equals("true");
-			JSONArray moviesData = (JSONArray) currentUser.get("movies");
-			int balance = Integer.parseInt(currentUser.get("balance").toString());
-			rentalMovieUser userToAdd = new rentalMovieUser(userName, password, country, isAdmin);
-			userToAdd.addBalance(balance);
-			for (int j = 0; j < moviesData.size(); j++) {
-				userToAdd.addMovie(movieDataBase.getMovie(currentUser.get("name").toString()));
-			}
-			userDataBase.addUser(userToAdd);
-
-		}
-	}
-
+	
 	public static void parseMoviesJSON() {
 		JSONParser parser = new JSONParser();
 		File srcFile = new File("Database/example_Movies.json");
@@ -95,9 +52,5 @@ public class jsonParser {
 			Movie movieToAdd = new Movie(id, name, price, bannedCountriesList, availableAmount, totalAmount);
 			movieDataBase.addMovie(movieToAdd);
 		}
-	}
-	
-	public static void main(String[] args) {
-		//TODO
 	}
 }
--- src/main/java/bgu/spl181/net/impl/usersParser.java
+++ src/main/java/bgu/spl181/net/impl/usersParser.java
@@ -1,20 +1,17 @@
 package bgu.spl181.net.impl;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.LinkedList;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
-public class jsonParser {
+public class usersParser {
 
 	private static MovieDataBase movieDataBase = MovieDataBase.getInstance();
 	private static UsersDataBase userDataBase = UsersDataBase.getInstance();
@@ -57,47 +54,5 @@ public class jsonParser {
 			userDataBase.addUser(userToAdd);
 
 		}
-	}
-
-	public static void parseMoviesJSON() {
-		JSONParser parser = new JSONParser();
-		File srcFile = new File("Database/example_Movies.json");
-		Reader reader = null;
-		try {
-			reader = new FileReader(srcFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		Object jsonObj = null;
-		try {
-			jsonObj = parser.parse(reader);
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		}
-
-		JSONObject jsonObject = (JSONObject) jsonObj;
-		JSONArray moviesData = (JSONArray) jsonObject.get("movies");
-
-		for (int i = 0; i < moviesData.size(); i++) {
-
-			JSONObject currentMovie = (JSONObject) moviesData.get(i);
-			int id = Integer.parseInt(currentMovie.get("id").toString());
-			String name = currentMovie.get("name").toString();
-			int price = Integer.parseInt(currentMovie.get("price").toString());
-			int availableAmount = Integer.parseInt(currentMovie.get("availableAmount").toString());
-			int totalAmount = Integer.parseInt(currentMovie.get("totalAmount").toString());
-			JSONArray bannedCountriesData = (JSONArray) currentMovie.get("bannedCountries");
-			LinkedList<String> bannedCountriesList = new LinkedList<String>();
-			for (int j = 0; j < bannedCountriesData.size(); i++)
-				bannedCountriesList.add(bannedCountriesData.get(i).toString());
-
-			Movie movieToAdd = new Movie(id, name, price, bannedCountriesList, availableAmount, totalAmount);
-			movieDataBase.addMovie(movieToAdd);
-		}
-	}
-	
-	public static void main(String[] args) {
-		//TODO
 	}
 }
